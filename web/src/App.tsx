630dde4dc1c4abcac436c9e39b1c4b87feccba2d
GustavoTreccia <g.treccia@outlook.com>
2020-10-22 16:22:14 -0300

    segundo commit estilo

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import './styles/global.css';
 
 
 function App() {
   return (
-    <div className="App">
+    <div className="frame">
+      <div className="tela">
+        <div className="fonte">
+          <h1>Teste_</h1>
+
+          <p>Olá! Me chamo Gustavo Treccia Guirado e esse é meu site!</p>
+        </div>
+      </div>
       
     </div>
   );
